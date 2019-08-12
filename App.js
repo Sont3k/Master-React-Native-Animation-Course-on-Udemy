681432853cfacccb10e644b7578fc0d31f98112b
Sont3k <markus.residence@gmail.com>
2019-08-12 09:30:38 +0300

    Added divide lesson

--- App.js
+++ App.js
@@ -4,23 +4,46 @@
  */
 
 import React from "react";
-import { View, StyleSheet, Animated } from "react-native";
+import {
+    View,
+    StyleSheet,
+    Animated,
+    TouchableWithoutFeedback
+} from 'react-native';
 
 export default class App extends React.Component {
   state = {
-    animation: new Animated.Value(0)
+    animation: new Animated.Value(100)
   };
 
-  animatedStyle = {};
+  startAnimation() {
+    Animated.timing(this.state.animation, {
+      toValue: 600,
+      duration: 1500
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: 1500
+      }).start();
+    });
+  }
+
+  arbitraryValue = 50;
+  // newAnimation = Animated.add(this.state.animation, this.arbitraryValue);
+  newAnimation = Animated.divide(this.state.animation, this.arbitraryValue);
+
+  animatedStyle = {
+    transform: [{ translateY: this.newAnimation }]
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View
-          style={[styles.box, this.animatedStyle]}
-        >
-          <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
-        </Animated.View>
+        <TouchableWithoutFeedback onPress={() => this.startAnimation()}>
+          <Animated.View style={[styles.box, this.animatedStyle]}>
+            <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
