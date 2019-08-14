461a3180e596481619d097d7703b84c0a004d254
Sont3k <markus.residence@gmail.com>
2019-08-14 10:07:04 +0300

    Parallel lesson

--- App.js
+++ App.js
@@ -4,23 +4,55 @@
  */
 
 import React from "react";
-import { View, StyleSheet, Animated } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Animated,
+  TouchableWithoutFeedback
+} from "react-native";
 
 export default class App extends React.Component {
   state = {
-    animation: new Animated.Value(0)
+    colorAnimation: new Animated.Value(0),
+    scaleAnimation: new Animated.Value(1)
   };
 
-  animatedStyle = {};
+  handlePress() {
+    Animated.parallel([
+      Animated.timing(this.state.colorAnimation, {
+        toValue: 1,
+        duration: 500
+      }),
+
+      Animated.timing(this.state.scaleAnimation, {
+        toValue: 2,
+        duration: 300
+      })
+    ]).start();
+  }
+
+  backgroundInterpolate = this.state.colorAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["rgb(255, 99, 71)", "rgb(99, 71, 255)"]
+  });
+
+  boxStyle = {
+    backgroundColor: this.backgroundInterpolate,
+    transform: [
+      {
+        scale: this.state.scaleAnimation
+      }
+    ]
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View
-          style={[styles.box, this.animatedStyle]}
-        >
-          <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
-        </Animated.View>
+        <TouchableWithoutFeedback onPress={() => this.handlePress()}>
+          <Animated.View style={[styles.box, this.boxStyle]}>
+            <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
