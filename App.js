f3a8e6bcc9dcd2645fdeb89dee3f1503ee3beeef
Sont3k <markus.residence@gmail.com>
2019-08-19 10:04:46 +0300

    Finished lesson

--- App.js
+++ App.js
@@ -4,23 +4,61 @@
  */
 
 import React from "react";
-import { View, StyleSheet, Animated } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Animated,
+  TouchableWithoutFeedback
+} from "react-native";
 
 export default class App extends React.Component {
   state = {
     animation: new Animated.Value(0)
   };
 
-  animatedStyle = {};
+  startAnimation() {
+    Animated.timing(this.state.animation, {
+      toValue: 1,
+      duration: 1500
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 2,
+        duration: 300
+      }).start();
+    });
+  }
+
+  animatedInterpolate = this.state.animation.interpolate({
+    inputRange: [0, 1, 2], // always must be increasing
+    outputRange: [0, 300, 0]
+  });
+
+  interpolatedInterpolate = this.animatedInterpolate.interpolate({
+    inputRange: [0, 300],
+    outputRange: [1, 0.5]
+  });
+
+  translateXInterpolate = this.animatedInterpolate.interpolate({
+    inputRange: [0, 30, 50, 80, 100, 150, 299, 300],
+    outputRange: [0, -30, -50, 80, -100, 300, 0, -100]
+  });
+
+  animatedStyle = {
+    transform: [
+      { translateY: this.animatedInterpolate },
+      { translateX: this.translateXInterpolate }
+    ],
+    opacity: this.interpolatedInterpolate
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View
-          style={[styles.box, this.animatedStyle]}
-        >
-          <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
-        </Animated.View>
+        <TouchableWithoutFeedback onPress={() => this.startAnimation()}>
+          <Animated.View style={[styles.box, this.animatedStyle]}>
+            <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
