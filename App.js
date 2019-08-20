911490ce5dd3e1488d643aba65771021140c9365
Sont3k <markus.residence@gmail.com>
2019-08-20 09:32:08 +0300

    Master improvement

--- App.js
+++ App.js
@@ -4,23 +4,30 @@
  */
 
 import React from "react";
-import { View, StyleSheet, Animated } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Animated,
+  TouchableWithoutFeedback
+} from "react-native";
 
 export default class App extends React.Component {
   state = {
     animation: new Animated.Value(0)
   };
 
+  startAnimation = () => {};
+
   animatedStyle = {};
 
   render() {
     return (
       <View style={styles.container}>
-        <Animated.View
-          style={[styles.box, this.animatedStyle]}
-        >
-          <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
-        </Animated.View>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, this.animatedStyle]}>
+            <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
       </View>
     );
   }
