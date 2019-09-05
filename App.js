bbb1614c31f9e5e2d5143a0bf53a9ac912db3c01
Sont3k <markus.residence@gmail.com>
2019-09-05 09:30:38 +0300

    Finished lesson

--- App.js
+++ App.js
@@ -6,27 +6,62 @@
 import React from "react";
 import {
   View,
+  Text,
   StyleSheet,
   Animated,
   TouchableWithoutFeedback
 } from "react-native";
 
+import { interpolateNumber, interpolateRgb } from "d3-interpolate";
+
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.positionInterpolate = interpolateNumber(0, 200);
+    this.colorInterpolate = interpolateRgb(
+      "rgb(255, 99, 71)",
+      "rgb(99, 71, 255)"
+    );
+
+    this.state.animation.addListener(({ value }) => {
+      const position = this.positionInterpolate(value);
+      const color = this.colorInterpolate(value);
+
+      const style = [
+        styles.box,
+        {
+          backgroundColor: color,
+          transform: [{ translateY: position }]
+        }
+      ];
+
+      this._view.setNativeProps({ style });
+    });
+  }
+
   state = {
     animation: new Animated.Value(0)
   };
 
   startAnimation = () => {};
 
+  handlePress = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 1,
+      duration: 500
+    }).start();
+  };
+
   animatedStyle = {};
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback onPress={this.startAnimation}>
-          <Animated.View style={[styles.box, this.animatedStyle]}>
-            <Animated.Text style={styles.textStyle}>Regular Box</Animated.Text>
-          </Animated.View>
+        <TouchableWithoutFeedback onPress={this.handlePress}>
+          <View style={styles.box} ref={view => (this._view = view)}>
+            <Text style={styles.textStyle}>Regular Box</Text>
+          </View>
         </TouchableWithoutFeedback>
       </View>
     );
